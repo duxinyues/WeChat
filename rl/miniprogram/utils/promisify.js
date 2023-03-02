455444e75fc34208f7e4024ce3a51d503b350634
duxinyues <yongyuan253015@gmail.com>
2023-03-02 22:11:20 +0800

    fix:微信小程序

--- rl/miniprogram/utils/promisify.js
+++
@@ -1,7 +0,0 @@
-module.export = (api) => {
-  return (options, ...params) => {
-    return new Promise((resolve, reject) => {
-      api(Object.assign({}, options, { success: resolve, fail: reject }), ...params);
-    })
-  }
-}
